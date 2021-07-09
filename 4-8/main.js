6dd4b579e7c17cee918f5ea180ee14c3bb5cdbb6
Shimizu-Yuuki <yuuki23112@gmail.com>
2021-07-09 12:29:22 +0900

    Update main.js

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,18 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+let arr=[];
+let str2="こはる";
+let str3="はるこ";
+let koharu=Number([]);
+let haruko=Number([]);
+
+for(let i=0;i<str.length;i++){
+  //arr.push(str[i]);
+  if(str[i]+str[i+1]+str[i+2]===str2){
+    koharu +=1;
+  }else if(str[i]+str[i+1]+str[i+2]===str3){
+    haruko +=1;
+  }
+}
+//console.log(arr);
+console.log(`こはる:${koharu}`);
+console.log(`はるこ:${haruko}`);
